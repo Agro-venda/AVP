c161cfba264fe3eae7516ea4bc96fba21ebca72a
Lucas de Souza <lucasouzaweb@hotmail.com>
2018-06-17 08:31:30 -0300

    fix db bugs TODO: improve listview fertilizantes

--- app/src/main/java/br/com/teste22/avptec/listViews/Fertilizantes.java
+++ app/src/main/java/br/com/teste22/avptec/listViews/Fertilizantes.java
@@ -5,65 +5,63 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
 
 import java.util.ArrayList;
-import java.util.List;
 
-import br.com.teste22.avptec.Model.ProdutoClass;
 import br.com.teste22.avptec.R;
 
 public class Fertilizantes extends AppCompatActivity {
-    private ListView listProdutos;
-
-    FirebaseDatabase firebaseDatabase;
-    DatabaseReference databaseReference;
-
-    private List<ProdutoClass> listPessoa = new ArrayList<ProdutoClass>();
-    private ArrayAdapter<ProdutoClass> arrayAdapterPessoa;
-    ProdutoClass pessoaSelecionada = null;
+    Firebase objetoRef;
+    ListView minhaLista;
+    ArrayList<String>minhaListaDeTarefas = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fertilizantes);
 
-        listProdutos = (ListView)findViewById(R.id.listProdutos);
+        Firebase.setAndroidContext(this);
+        minhaLista = (ListView)findViewById(R.id.listProdutos);
 
 
-        inicializarFirebase();
-        eventoDatabase();
-    }
-    private void eventoDatabase() {
-        databaseReference.child("ProdutoClass").addValueEventListener(new ValueEventListener() {
+        objetoRef = new Firebase("https://agrovendas-4caed.firebaseio.com/");
+
+        Firebase novaRef = objetoRef.child("produtos");
+
+        final ArrayAdapter<String>adaptador = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, minhaListaDeTarefas);
+        minhaLista.setAdapter(adaptador);
+
+        novaRef.addChildEventListener(new com.firebase.client.ChildEventListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                listPessoa.clear();
-                for (DataSnapshot objSnapshot:dataSnapshot.getChildren()){
-                    ProdutoClass p = objSnapshot.getValue(ProdutoClass.class);
-                    listPessoa.add(p);
-                }
-                arrayAdapterPessoa = new ArrayAdapter<ProdutoClass>(Fertilizantes.this,
-                        android.R.layout.simple_list_item_1,listPessoa);
-                listProdutos.setAdapter(arrayAdapterPessoa);
+            public void onChildAdded(com.firebase.client.DataSnapshot dataSnapshot, String s) {
+                String message = dataSnapshot.getValue(String.class);
+                minhaListaDeTarefas.add(message);
+                adaptador.notifyDataSetChanged();
             }
 
             @Override
-            public void onCancelled(DatabaseError databaseError) {
+            public void onChildChanged(com.firebase.client.DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(com.firebase.client.DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(com.firebase.client.DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
 
             }
         });
     }
 
-    private void inicializarFirebase() {
-        FirebaseApp.initializeApp(Fertilizantes.this);
-        firebaseDatabase = FirebaseDatabase.getInstance();
-        // firebaseDatabase.setPersistenceEnabled(true);
-        databaseReference = firebaseDatabase.getReference();
-    }
+
 }
